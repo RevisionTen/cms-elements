7288482b2f5175f0b8373db05bf4619e75fe08fc
Cornelius K <kahns@bdrops.de>
2020-11-18 11:34:53 +0100

    Added max width option to image elements

--- Elements/BetterDefault/ImageSettings.php
+++ Elements/BetterDefault/ImageSettings.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace RevisionTen\CmsElements\Elements\BetterDefault;
 
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class ImageSettings extends \RevisionTen\CMS\Form\Elements\ImageSettings
@@ -16,6 +17,12 @@ class ImageSettings extends \RevisionTen\CMS\Form\Elements\ImageSettings
      */
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $builder->add('setMaxWidth', CheckboxType::class, [
+            'label' => 'element.label.setMaxWidth',
+            'translation_domain' => 'cms',
+            'required' => false,
+        ]);
+
         parent::buildForm($builder, $options);
 
         $builder->remove('scaling');
